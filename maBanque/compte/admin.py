f773179e1306010b698e006e286486fc5d2e5787
devhyacinthe <yacinthedalmeida2002@gmail.com>
2022-07-13 19:30:59 +0000

    Creation des models et migrations

--- maBanque/compte/admin.py
+++ maBanque/compte/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from compte.models import Client, Operation, Compte
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Operation)
+admin.site.register(Compte)
--- maBanque/compte/models.py
+++ maBanque/compte/models.py
@@ -1,3 +1,25 @@
+
 from django.db import models
+from django.utils import timezone
+import uuid
 
 # Create your models here.
+
+class Client(models.Model):
+    idClient = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+
+class Operation(models.Model):
+    date = models.DateTimeField()
+    type = models.CharField(max_length=20)
+    montant = models.IntegerField()
+
+class Compte(models.Model):
+    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    numero = models.IntegerField()
+    client = models.ForeignKey("Client", on_delete=models.CASCADE)
+    solde = models.IntegerField()
+    operations = models.ForeignKey("Operation", on_delete=models.CASCADE)
+
+
